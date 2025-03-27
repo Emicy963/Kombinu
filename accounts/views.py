225a9606a1e6fa9250436b2d76155f00c64b06c1
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-03-27 21:38:25 +0100

    Project Ready to deploy

---
+++ accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .form import CustomUserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Conta Criada com Sucesso.')
+            return redirect('dasboard')
+    else:
+        form = CustomUserCreationForm()
+        return render(request, 'register.html', {'form': form})
+    
+def login_view(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dasboard')
+        else:
+            messages.error(request, 'Nome ou senha incorreta')
+    else:
+        return render(request, 'login.html')
+    
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+def start_view(request):
+    return render(request, 'start_login.html')
---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from accounts.views import start_view
+from dasboard.views import home_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', start_view, name='home'),
+    path('auth/', include('accounts.urls')),
+    path('dasbord/', home_view, name='dasboard'),
+]
