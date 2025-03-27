ac9c7c3600556e692607bfac624e5ec482366535
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-03-27 23:30:22 +0100

    Chance the start_login.html

--- accounts/views.py
+++ accounts/views.py
@@ -24,8 +24,7 @@ def login_view(request):
             return redirect('dashboard')
         else:
             messages.error(request, 'Nome ou senha incorreta')
-    else:
-        return render(request, 'login.html')
+    return render(request, 'login.html')
     
 def logout_view(request):
     logout(request)
