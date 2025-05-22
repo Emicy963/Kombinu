844eaa4b578de4080498c64abfb6896123fe0993
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-22 03:27:07 +0100

    Create a overview html to dasboard

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from accounts.views import start_view
-from dasboard.views import home_view
+from dasboard.views import home_view, dashboard_overview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
-    path('login/', start_view, name='login')
+    path('dasboard/', dashboard_overview, name='overview'),
+    path('login/', start_view, name='login'),
     #path('accounts/', include('accounts.urls')),
     #path('dashboard/', home_view, name='dashboard'),
 ]
