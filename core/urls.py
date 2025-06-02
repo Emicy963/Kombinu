b84b436cb4a5f3d9545b114257e43ed874668a8e
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-06-02 23:08:48 +0100

    Config urls for Documentation

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from accounts.views import start_view
 from dasboard.views import home_view, dashboard_overview, termos_condicoes
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,6 +18,11 @@ urlpatterns = [
     # API urls
     path('api/auth/', include('accounts.urls_api')),
     path('api/courses/', include('courses.urls')),
+
+    # Documentation
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 if settings.DEBUG:
