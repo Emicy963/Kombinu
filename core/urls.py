5bb2752d018e49a50b3fc6e3b9c3470df554f325
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-24 19:26:06 +0100

    feat(urls): Refatoring the main urls

--- core/urls.py
+++ core/urls.py
@@ -1,25 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import login_view, register_view, logout_view
-from dasboard.views import home_view, dashboard_overview, termos_condicoes
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'),
-    path('dashboard/', dashboard_overview, name='overview'),
-    path('temos_condicoes/', termos_condicoes, name='termos'),
-    path('login/', login_view, name='login'),
-    path('register/', register_view, name='register'),
-    path('logout/', logout_view, name='logout'),
-    #path('accounts/', include('accounts.urls')),
-    #path('dashboard/', home_view, name='dashboard'),
-
-    # API urls
-    path('api/auth/', include('accounts.urls_api')),
-    path('api/courses/', include('courses.urls')),
 
     # Documentation
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
