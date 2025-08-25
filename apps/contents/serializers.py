e90c03d511d8d909ed2b923d91491ef04a7c5503
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-25 15:11:20 +0100

    feat(serializer): Create serializer for content

--- apps/contents/serializers.py
+++ apps/contents/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from contents.models import Content
 
 
 class CreatorSerializer(serializers.ModelSerializer):
@@ -11,3 +12,13 @@ class CreatorSerializer(serializers.ModelSerializer):
     
     def get_name(self, obj):
         return f"{obj.first_name} {obj.last_name}".strip() or obj.email
+
+class ContentSerializer(serializers.ModelSerializer):
+    creator = CreatorSerializer(read_only=True)
+    has_quiz = serializers.BooleanField(read_only=True)
+    quiz_id = serializers.UUIDField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = "__all__"
+        read_only_fields = ("id", "creator", "created_at", "has_quiz", "quiz_id")
