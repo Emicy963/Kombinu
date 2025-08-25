a53fb65ba736d47c8159fba0a92235277af6caec
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-25 11:56:40 +0100

    feat(views): Create APIView for Register User

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from .models import CustomUser
 
-class UserRegistratioSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
     user_type = serializers.ChoiceField(choices=CustomUser.USER_TYPE_CHOICES)
 
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+class RegisterView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                "message": "User created successfuly.",
+                "user_id": str(user.id),
+                "email": user.email
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
