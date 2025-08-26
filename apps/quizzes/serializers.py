e2f0342ce50b584c961ba01f84db429394ac4391
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-26 13:04:54 +0100

    feat(serializers): Create serializer for question

--- apps/quizzes/serializers.py
+++ apps/quizzes/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from .models import Option
+from .models import Option, Question
 
 
 class OptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Option
         fields = ("pk", "text")
+        read_only_fields = ("pk")
+
+class QuestionSerializer(serializers.ModelSerializer):
+    options = OptionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = ("pk", "question_text", "options")
         read_only_fields = ("pk")
