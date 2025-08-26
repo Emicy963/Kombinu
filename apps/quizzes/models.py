eb5615017c51ff62f6f5936661fcf85354cffe82
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-26 12:46:53 +0100

    feat(models): Create model for Question

--- apps/quizzes/models.py
+++ apps/quizzes/models.py
@@ -8,3 +8,10 @@ class Quiz(models.Model):
 
     def __str__(self):
         return self.title
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="questions")
+    question_test = models.TextField()
+
+    def __str__(self):
+        return self.question_test[:50]
