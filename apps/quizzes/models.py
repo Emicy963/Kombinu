1e2b2eedf507bf641411a6560de7369fcbbf0549
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-26 12:43:48 +0100

    feat(models): Create the quiz model

--- apps/quizzes/models.py
+++ apps/quizzes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from apps.contents.models import Content
 
-# Create your models here.
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.OneToOneField(Content, on_delete=models.CASCADE, related_name="quiz")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
