01049848b3d2bd634e7b3472e73237381192c0cb
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-27 15:06:20 +0100

    feat(urls): Create endpoints for quiz app

---
+++ apps/quizzes/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import QuizDetailView, QuizGenerationView, QuizSubmissionView
+
+
+urlpatterns = [
+    path('<uuid:id>/', QuizDetailView.as_view(), name='quiz-detail'),
+    path('<uuid:quiz_id>/submit/', QuizSubmissionView.as_view(), name='quiz-submit'),
+    path('contents/<uuid:content_id>/generate-quiz/', QuizGenerationView.as_view(), name='quiz-generate'),
+]
