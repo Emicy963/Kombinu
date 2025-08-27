d2078d937dfcecc285028e15acd86785760dbce4
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-27 14:08:00 +0100

    feat(views): Create the view class for show Quiz Detail

--- apps/quizzes/views.py
+++ apps/quizzes/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .serializers import QuizDetailSerializer
+from .models import Quiz
 
-# Create your views here.
+
+class QuizDetailView(generics.RetrieveAPIView):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizDetailSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = "pk"
