039452b36083959ec893656c9951b5b50cefc02d
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-09-01 08:32:28 +0100

    feat(secure): Reconfig get the env elements.

--- core/settings.py
+++ core/settings.py
@@ -1,23 +1,24 @@
 import os
 from pathlib import Path
-from decouple import Csv, config
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # import dj_database_url as db_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", cast=bool, default=False)
+DEBUG = os.getenv("DEBUG", "False")
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default=[])
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
@@ -35,7 +36,7 @@ THIRD_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
-    'drf_spectacular',
+    "drf_spectacular",
 ]
 
 LOCAL_APPS = [
@@ -88,6 +89,12 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 # DATABASES = {
 #     'default': db_url.config(default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR}/db.sqlite3'))
 # }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
