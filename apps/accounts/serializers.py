7fd80a5ad09ba2455dece6faac1f6ff2d1377322
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-09-04 16:40:09 +0100

    feat(accounts): Refatoring the project

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from .models import CustomUser
 
+
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
     user_type = serializers.ChoiceField(choices=CustomUser.USER_TYPE_CHOICES)
@@ -9,14 +10,15 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ("email", "password", "user_type")
-    
+
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             email=validated_data["email"],
             password=validated_data["password"],
-            user_type=validated_data["user_type"]
+            user_type=validated_data["user_type"],
         )
         return user
+
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserProfileSerializer, UserRegistrationSerializer
 
+
 class RegisterView(APIView):
     permission_classes = []
 
@@ -11,28 +12,38 @@ class RegisterView(APIView):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            return Response({
-                "message": "User created successfuly.",
-                "user_id": str(user.id),
-                "email": user.email
-            }, status=status.HTTP_201_CREATED)
+            return Response(
+                {
+                    "message": "User created successfuly.",
+                    "user_id": str(user.id),
+                    "email": user.email,
+                },
+                status=status.HTTP_201_CREATED,
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class LoginView(APIView):
     permission_classes = []
 
     def post(self, request):
         from django.contrib.auth import authenticate
+
         email = request.data.get("email")
         password = request.data.get("password")
         user_instance = authenticate(request, username=email, password=password)
         if user_instance is not None:
             refresh = RefreshToken.for_user(user_instance)
             user_serializer = UserProfileSerializer(user_instance)
-            return Response({
-                "access_token": str(refresh.acess_token),
-                "refresh_token": str(refresh),
-                "user": user_serializer.data
-            }, status=status.HTTP_200_OK)
+            return Response(
+                {
+                    "access_token": str(refresh.acess_token),
+                    "refresh_token": str(refresh),
+                    "user": user_serializer.data,
+                },
+                status=status.HTTP_200_OK,
+            )
         else:
-            return Response({"error": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response(
+                {"error": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED
+            )
