42f18f21a7bd958db6abaf1da79605124447f11a
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-06 23:27:09 +0100

    fix(accounts): Add email validation on serializer for register new user.

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -6,13 +6,20 @@ from .models import CustomUser
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
     user_type = serializers.ChoiceField(choices=CustomUser.USER_TYPE_CHOICES)
+    email = serializers.EmailField(required=True)
 
     class Meta:
         model = CustomUser
         fields = ("email", "password", "user_type")
 
+    def validate_email(self, value):
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return value
+
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
+            username=validated_data["email"],
             email=validated_data["email"],
             password=validated_data["password"],
             user_type=validated_data["user_type"],
