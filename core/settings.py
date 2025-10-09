58a17c14e496d4ea28497817050aa520c9438018
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-10 00:14:05 +0100

    fix(settings): Add the cors headers for security.

--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,7 @@ LOCAL_APPS = [
 INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
